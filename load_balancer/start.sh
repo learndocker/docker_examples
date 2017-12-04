05564dda9e6c71093f92d21f0ca11cb3b8d00427
Julian Fahrer <julian@fahrer.net>
2017-12-04 15:13:10 +0100

    Make sure that nginx becomes PID 1 [refs #49b2f3dafcad]

--- load_balancer/start.sh
+++ load_balancer/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 envsubst '$PROXY_PROTOCOL,$PROXY_UPSTREAM' < /etc/nginx/sites-available/default.template > /etc/nginx/sites-available/default
-nginx -g 'daemon off;'
+exec nginx -g 'daemon off;'
