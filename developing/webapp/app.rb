6119d5896234854c8091aa28cd5c8b109eafd570
Julian Fahrer <julian@fahrer.net>
2018-01-12 13:33:43 +0100

    Display a list of requests retrieved from the database [refs #b7400db79a80]

--- developing/webapp/app.rb
+++ developing/webapp/app.rb
@@ -9,6 +9,33 @@ get '/' do
                              ENV['POSTGRES_USER'], ENV['POSTGRES_PASSWORD'])
   sql = "INSERT INTO requests (ip, path, host, requested_at) VALUES ($1, $2, $3, $4)"
   res  = conn.exec(sql, [request.ip, '/', Socket.gethostname, Time.now])
+
+  sql = "SELECT ip, path, host, requested_at FROM requests ORDER BY id DESC LIMIT 25;"
+  res  = conn.exec(sql)
+
   conn.finish
-  'Hello, World!'
+
+  requests_table = res.map do |row|
+    values = row.map do |column|
+      "<td>#{column[1]}</td>"
+    end.join('')
+    "<tr>#{values}</tr>"
+  end.join('')
+
+  <<-BODY
+    <html>
+      <body>
+        <h1>The requests</h1>
+        <table style="width: 100%;" border="1">
+          <tr>
+            <th>IP</th>
+            <th>Path</th>
+            <th>Container</th>
+            <th>Timestamp</th>
+          </tr>
+          #{requests_table}
+        </table>
+      </body>
+    </html>
+  BODY
 end
