0faad1665e71cbdb41725ecb2f65f7b793265e68
Julian Fahrer <julian@fahrer.net>
2018-01-12 12:07:19 +0100

    Implement the second assignment in ruby [refs #efdf2821ab7f]

--- developing/examples/webapp/ruby/app.rb
+++ developing/examples/webapp/ruby/app.rb
@@ -4,5 +4,12 @@ set :bind, '0.0.0.0'
 set :port, 9494
 
 get '/' do
+  File.open('/tmp/requests.txt', 'a') do |file|
+    file.puts "IP: #{request.ip}"
+    file.puts "Hostname: #{Socket.gethostname}"
+    file.puts "Path: #{request.path}"
+    file.puts "Timestamp: #{Time.now}"
+    file.puts '------------'
+  end
   'Hello World!'
 end
