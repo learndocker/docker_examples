c66df67141c0a64c0e4b2a13b77234e36623040a
Julian Fahrer <julian@fahrer.net>
2018-01-16 19:25:55 +0100

    Update the swarm-demo-frontend to v5 [refs %fe270e7659a4]

--- swarm/swarm-demo-frontend/app.rb
+++ swarm/swarm-demo-frontend/app.rb
@@ -1,15 +1,41 @@
 require 'sinatra'
+require 'erb'
+require 'pg'
 
 set :bind, '0.0.0.0'
 set :port, 8080
 
+AppState = {}
+AppState[:total_requests] = 0
+
 get '/' do
-  <<-BODY
-    <html>
-      <body>
-        <h1>Docker Swarm Demo</h1>
-        <p>This page was served by <b>#{Socket.gethostname}</b></p>
-      </body>
-    </html>
-  BODY
+  AppState[:total_requests] += 1
+
+  manager = RequestManager.new
+  manager.store_request(request.ip, '/', Socket.gethostname, Time.now)
+
+  erb :index, locals: { requests: manager.get_requests(25), total_requests: AppState[:total_requests] }
+end
+
+class RequestManager
+
+  def store_request(ip, path, host, requested_at)
+    sql = "INSERT INTO requests (ip, path, host, requested_at) VALUES ($1, $2, $3, $4)"
+    res  = conn.exec(sql, [ip, path, host, requested_at])
+  end
+
+  def get_requests(limit = 100)
+    sql = "SELECT ip, path, host, requested_at FROM requests ORDER BY id DESC LIMIT $1;"
+    res  = conn.exec(sql, [limit])
+    res.map do |row|
+      OpenStruct.new(row.to_h)
+    end
+  end
+
+  private
+
+  def conn
+    @conn ||= PG::Connection.open(ENV['POSTGRES_HOST'], 5432, '', '', ENV['POSTGRES_DB'],
+                                  ENV['POSTGRES_USER'], ENV['POSTGRES_PASSWORD'])
+  end
 end
