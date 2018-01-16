913ee36520c5a17c9f817a9405fb2da365234407
Julian Fahrer <julian@fahrer.net>
2018-01-16 19:58:59 +0100

    Change the bg color to grey for v2 [refs %608d12517eb6]

--- swarm/swarm-demo-frontend/app.rb
+++ swarm/swarm-demo-frontend/app.rb
@@ -6,7 +6,7 @@ set :port, 8080
 get '/' do
   <<-BODY
     <html>
-      <body>
+      <body style="background: grey;">
         <h1>Docker Swarm Demo</h1>
         <p>This page was served by <b>#{Socket.gethostname}</b></p>
       </body>
