e5222a39e38f1bfe67c6ae41e9514ef83adbcae4
Julian Fahrer <julian@fahrer.net>
2018-01-16 19:57:35 +0100

    Add version 1 of the swarm frontend [refs %929d116b6795]

---
+++ swarm/swarm-demo-frontend/app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+set :bind, '0.0.0.0'
+set :port, 8080
+
+get '/' do
+  <<-BODY
+    <html>
+      <body>
+        <h1>Docker Swarm Demo</h1>
+        <p>This page was served by <b>#{Socket.gethostname}</b></p>
+      </body>
+    </html>
+  BODY
+end
