8e506bf14baf37426a7dbbc44cad78ec9426cc8e
Julian Fahrer <julian@fahrer.net>
2018-01-16 20:01:06 +0100

    Resolve the error and make the bg white again for v4 [refs %52bc410e062d]

--- swarm/swarm-demo-frontend/app.rb
+++ swarm/swarm-demo-frontend/app.rb
@@ -3,12 +3,10 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port, 8080
 
-raise "Simulating an error"
-
 get '/' do
   <<-BODY
     <html>
-      <body style="background: grey;">
+      <body>
         <h1>Docker Swarm Demo</h1>
         <p>This page was served by <b>#{Socket.gethostname}</b></p>
       </body>
