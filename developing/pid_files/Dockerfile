c9c223a4ce460c5384f7afb937aa5c2c6621f24b
Julian Fahrer <julian@fahrer.net>
2018-02-06 08:02:49 -0800

    Add the inital files for the PID file example [refs #f13c9c0c4d30]

---
+++ developing/pid_files/Dockerfile
@@ -0,0 +1,5 @@
+FROM ruby:2.4.3-alpine
+
+WORKDIR /app
+
+CMD ["/usr/local/bin/ruby", "/app/app.rb"]
---
+++ developing/pid_files/app.rb
@@ -0,0 +1,34 @@
+def pidfile
+  './tmp/app.pid'
+end
+
+def write_pid
+  puts "Writing my PID (#{Process.pid}) to #{pidfile}"
+  File.open(pidfile, "w") do |f|
+    f.write Process.pid
+  end
+end
+
+def running?
+  return false unless File.exists?(pidfile)
+  pid = File.read(pidfile)
+  Process.kill(0, pid.to_i)
+  pid
+rescue Errno::ESRCH
+  false
+end
+
+def do_work
+  while true
+    puts 'Working on solving the problem...'
+    sleep 1
+  end
+end
+
+if pid = running?
+  puts "Application is already running as #{pid}, exiting..."
+  exit 1
+else
+  write_pid
+  do_work
+end
